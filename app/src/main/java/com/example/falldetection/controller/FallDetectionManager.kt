923784c1c37b840b2fcc2d00c4bd6e8011989a4b
MariaEduardaMorais <meramorais12@gmail.com>
2025-02-08 16:43:26 -0300

    adding important commentaries

--- app/src/main/java/com/example/falldetection/controller/FallDetectionManager.kt
+++ app/src/main/java/com/example/falldetection/controller/FallDetectionManager.kt
@@ -30,8 +30,8 @@ class FallDetectionManager(
     // debounce e sensibilidade
     private var lastFallDetectionTime: Long = 0
     private val debounceTime = 5000
-    private val accelerationThreshold = 20.0f // limite de aceleração para detectar possível queda
-    private val gyroscopeThreshold = 8.0f // limite de giroscópio para confirmar queda
+    private val accelerationThreshold = 20.0f // limite de aceleração p/ detectar possível queda
+    private val gyroscopeThreshold = 8.0f // limite de giroscópio p/ confirmar queda
 
     private var possibleFallTime: Long = 0
     private var detectedAcceleration = false
